77ed1393eb2ae875c6598d27f780546051d7f748
PNR2 <94699808+PNR2@users.noreply.github.com>
2026-08-02 11:18:03 +0530

    Update SenManga.kt

--- src/ja/senmanga/src/eu/kanade/tachiyomi/extension/ja/senmanga/SenManga.kt
+++ src/ja/senmanga/src/eu/kanade/tachiyomi/extension/ja/senmanga/SenManga.kt
@@ -6,7 +6,7 @@ import eu.kanade.tachiyomi.source.model.Page
 import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
 import eu.kanade.tachiyomi.source.online.ParsedHttpSource
-import keiyoushi.utils.Source
+import keiyoushi.annotation.Source
 import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
 import okhttp3.Request
 import org.jsoup.nodes.Document
